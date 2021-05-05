bb26dfaa477f2167d07ca0d115bc3a22c6c511c5
Yori Mirano <yori.mirano@gmail.com>
2021-05-05 13:50:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+/**
+ * Watch files
+ */
+const fs = require('fs');
+const homedir = require('os').homedir();
+const config = require('./config.json');
+const baseDir = config.dir.replace('%userprofile%', homedir);
+const files = {
+  navRoute: baseDir + '/NavRoute.json'
+}
+
+
+// NavRoute
+let navRoute;
+
+fs.watchFile(files.navRoute, (curr, prev) => {
+  updateNavRouteFromFile();
+
+  if (io) {
+    io.emit('navroute', navRoute);
+  }
+});
+
+function updateNavRouteFromFile() {
+  const rawdata = fs.readFileSync(files.navRoute);
+  navRoute = JSON.parse(rawdata);
+  console.log('> navroute:', navRoute.Route.length, 'steps');
+}
+
+updateNavRouteFromFile();
+
+
+// Current System
+let currentSystem;
+let lastSystem;
+const glob = require('glob')
+const lineReader = require('line-reader');
+const lastLogFilePath = getLastLogFilePath();
+console.log('> log file: ' + lastLogFilePath);
+
+function getLastLogFilePath() {
+  return glob.sync(baseDir + '/*.log')
+    .map(name => ({name, ctime: fs.statSync(name).ctime}))
+    .sort((a, b) => b.ctime - a.ctime)[0].name
+}
+
+function readLastLogFile() {
+  lineReader.eachLine(lastLogFilePath, function(line, last) {
+    let log = JSON.parse(line);
+    
+    if (log.event === 'FSDJump' || log.event === 'Location') {
+      currentSystem = log.StarSystem;
+    }
+  
+    if (last && currentSystem && lastSystem !== currentSystem) {
+      lastSystem = currentSystem
+      console.log('> system: ', currentSystem);
+
+      if (io) {
+        io.emit('currentSystem', currentSystem);
+      }
+    }
+  });
+}
+
+fs.watchFile(lastLogFilePath, (curr, prev) => {
+  readLastLogFile();
+});
+
+readLastLogFile();
+
+
+/**
+ * Server
+ */
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('> a user connected');
+
+  if (navRoute) {
+    socket.emit('navroute', navRoute);
+  }
+
+  if (currentSystem) {
+    socket.emit('currentSystem', currentSystem);
+  }
+
+  socket.on('disconnect', () => {
+    console.log('> user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('> listening on *:3000');
+});
