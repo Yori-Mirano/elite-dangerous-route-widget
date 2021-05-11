957a17f5205cd26a2ced190a1df25b65d7f09728
Yori Mirano <yori.mirano@gmail.com>
2021-05-11 18:18:37 +0200

    refactor: renames server and client files

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/client.html');
 });
 
 io.on('connection', (socket) => {
